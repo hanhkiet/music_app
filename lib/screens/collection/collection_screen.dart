4b7336c930ef2672b481973d2634a9c90442b724
kite-user <huynhanhkiet179@gmail.com>
2022-11-06 09:44:32 +0700

    collection screen ui

--- lib/screens/collection/collection_screen.dart
+++ lib/screens/collection/collection_screen.dart
@@ -1,10 +1,150 @@
-import 'package:flutter/material.dart';
+import 'dart:convert';
 
-class CollectionScreen extends StatelessWidget {
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:music_app/models/collection_model.dart';
+import 'package:music_app/models/song_model.dart';
+import 'package:music_app/screens/collection/collection_controller.dart';
+import 'package:music_app/services/firebase_functions.dart';
+import 'package:music_app/services/firebase_storage.dart';
+
+class CollectionScreen extends GetView<CollectionController> {
   const CollectionScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+          colors: [
+            const Color.fromARGB(255, 0, 150, 158).withOpacity(.8),
+            const Color.fromARGB(255, 0, 242, 255).withOpacity(.8),
+          ],
+        ),
+      ),
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+        ),
+        backgroundColor: Colors.transparent,
+        body: SingleChildScrollView(
+          padding: const EdgeInsets.all(20),
+          child: Column(
+            children: [
+              const CollectionInformation(),
+              const SizedBox(height: 30),
+              FutureBuilder(
+                future: FunctionsService.callFunction(
+                    'getSongsFromCollection', {"id": Get.arguments.id}),
+                builder: (context, snapshot) {
+                  if (!snapshot.hasData) {
+                    return Container();
+                  }
+
+                  final data = snapshot.data!.data.map((e) => json.encode(e))
+                      as Iterable;
+                  final songs = Song.fromData(data);
+                  return SongList(songs: songs);
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class SongList extends StatelessWidget {
+  const SongList({
+    Key? key,
+    required this.songs,
+  }) : super(key: key);
+
+  final List<Song> songs;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      shrinkWrap: true,
+      physics: const NeverScrollableScrollPhysics(),
+      itemCount: songs.length,
+      itemBuilder: (context, index) {
+        return ListTile(
+          leading: Text(
+            '${index + 1}',
+            style: Theme.of(context)
+                .textTheme
+                .bodyMedium!
+                .copyWith(fontWeight: FontWeight.bold),
+          ),
+          title: Text(
+            songs[index].name,
+            style: Theme.of(context)
+                .textTheme
+                .bodyLarge!
+                .copyWith(fontWeight: FontWeight.bold),
+          ),
+          trailing: const Icon(
+            Icons.more_vert,
+            color: Colors.white,
+          ),
+        );
+      },
+    );
+  }
+}
+
+class CollectionInformation extends StatelessWidget {
+  const CollectionInformation({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final Collection collection = Get.arguments;
+
+    return Column(
+      children: [
+        FutureBuilder(
+          future:
+              StorageService.getDownloadUrl('covers/${collection.coverUrl}'),
+          builder: (context, snapshot) {
+            if (!snapshot.hasData) {
+              return const CircularProgressIndicator();
+            }
+
+            String url = snapshot.data!;
+            return CachedNetworkImage(
+              imageUrl: url,
+              imageBuilder: (context, imageProvider) => Container(
+                width: MediaQuery.of(context).size.width * .6,
+                height: MediaQuery.of(context).size.width * .6,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: imageProvider,
+                    fit: BoxFit.cover,
+                  ),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              ),
+              placeholder: (context, url) => const CircularProgressIndicator(),
+            );
+          },
+        ),
+        const SizedBox(height: 30),
+        Text(
+          collection.name,
+          style: Theme.of(context)
+              .textTheme
+              .headlineSmall!
+              .copyWith(fontWeight: FontWeight.bold),
+        )
+      ],
+    );
   }
 }
